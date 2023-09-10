1cb45778e43b69f051d20432d79758a84287a3aa
Asliddin Amirov <asliddinamir2@gmail.com>
2023-09-10 21:38:24 +0800

    added back extra button on js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const nums = document.querySelectorAll('.num')
 const btn = document.querySelector('.submit')
+const backBtn = document.querySelector('.back')
 const selectedNumber = document.querySelector('.selected-number');
+const selectedNumber2 = document.querySelector('.selected-number2');
+const container1 = document.getElementById('container1')
+const container2 = document.getElementById('container2')
+
 
 const selected = nums.forEach((num) => {
     num.addEventListener('click', () => {
@@ -11,8 +16,6 @@ const selected = nums.forEach((num) => {
 
         // Add 'active' class to the clicked num element
         num.classList.add('active');
-
-        console.log(num.innerText);
     });
 });
 
@@ -20,11 +23,22 @@ btn.addEventListener('click', () => {
     const activeNum = document.querySelector('.num.active');
 
     if (activeNum) {
-        selectedNumber.textContent = `You selected: ${activeNum.innerText} out of 5`;
+        container1.classList.add('hidden');
+        container2.classList.remove('hidden')
+        selectedNumber2.textContent = `You selected ${activeNum.innerText} out of 5`;
     } else {
         selectedNumber.textContent = 'Please select a number first.';
     }
 });
+
+backBtn.addEventListener('click', () => {
+    container2.classList.add('hidden');
+    container1.classList.remove('hidden');
+
+    const activeNum2 = document.querySelector('.num.active')
+
+    activeNum2.classList.remove('active')
+})
 
 
 
